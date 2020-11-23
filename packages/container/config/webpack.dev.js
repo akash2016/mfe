33e05c3a7af7bbe5afff1380c87d2bb9e4e473fd
akash2016 <akash.verma@evivehealth.com>
2020-11-23 16:04:23 +0530

    created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -18,10 +17,7 @@ const devConfig = {
             },
             shared: packageJson.dependencies,
         }
-        ),
-        new HTMLWebpackPlugin({
-            template: './public/index.html'
-        })
+        )
     ]
 };
 
